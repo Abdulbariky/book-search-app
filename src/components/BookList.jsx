5587cfcce1d693aea74ddf398dae9536427a49cc
abdulbarik <abdulbariky000@gmail.com>
2024-05-01 12:07:37 +0300

    i have used the UseEffect Hook to call the getBooks function when the input state variable changes

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,12 +1,19 @@
-const BookList = ({books}) => {
-    return ( <div>
-        {books.map((book) =>(
-              <div className="book-container">
-                {book.volumeInfo.imageLinks && <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="" />}
-                             
-          </div>
-        ))}      
-    </div> );
+import React from 'react';
+
+const BookList = ({ books }) => {
+  if (!books) {
+    return null;
+  }
+
+  return (
+    <div>
+      {books.map((book) => (
+        <div className="book-container">
+          {book.volumeInfo.imageLinks && <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="" />}
+        </div>
+      ))}
+    </div>
+  );
 }
- 
+
 export default BookList;
--- src/components/SearchBook.jsx
+++ src/components/SearchBook.jsx
@@ -5,22 +5,31 @@ import BookList from "./BookList";
 
 const SearchBook = () => {
     const [books, setBooks] = useState([]);
+    const [input, setInput] = useState('');
 
     useEffect(() => {
         getBooks();
-    }, []);
+    }, [input]);
 
     const getBooks = async () => {
-        const res = await fetch('https://www.googleapis.com/books/v1/volumes?q=reactjs:keyes&key=AIzaSyDa1-TjhN4Loxp4-qQlo7eybpfbGsMIgYI');
+        const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}&key=AIzaSyDa1-TjhN4Loxp4-qQlo7eybpfbGsMIgYI`);
         const data = await res.json();
         console.log(data.items);
-        setBooks(data.items);
+        if(data.items){
+            setBooks(data.items);
+        }
+        
     }
 
     return (
-        <div>
-         <BookList books={books} />
-        </div>
+        <>              
+    <div className="input-container">
+        <input type="text" value={input} 
+        onChange={(e)=>{setInput(e.target.value); setInput(e.target.value);}}/>
+
+    </div>
+    <BookList books={books} />
+    </> 
     );
 }
 
