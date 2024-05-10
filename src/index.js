63927b4160e8770d889ba507ba66e16a5b9c3796
abdulbarik <abdulbariky000@gmail.com>
2024-05-10 20:03:07 +0300

    added index.js and index.css files

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import { AppProvider } from './context.';
+import './index.css';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
+import MyCollection from './pages/My Collection/myCollection';
+import Contact from './pages/Contact/Contact.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+          <Route path = "collection" element = {<MyCollection />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
+);
